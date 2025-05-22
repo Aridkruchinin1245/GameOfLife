e6d92fe28eb94f8cf0f2c35232884621b7fe9cc0
Aridkruchinin1245 <misakrucinin80@gmail.com>
2025-05-22 12:35:10 +0300

    need to be optimised and improve dying of cells

--- main.py
+++ main.py
@@ -3,62 +3,84 @@ from pygame.color import THECOLORS
 import sys
 import asyncio
 from random import randint
-
-a = -25
-b = -25
 pygame.init()
 screen = pygame.display.set_mode((1000,700))
 screen.fill(THECOLORS['white'])
-pygame.display.flip()
-
+lifecells = []
 class Cell:
-    # def CheckCells(cell):
-    #     rectangles = [pygame.Rect(cell.x+25,cell.y,25,25),pygame.Rect(cell.x-25,cell.y,25,25),pygame.Rect(cell.x,cell.y+25,25,25), pygame.Rect(cell.x,cell.y-25,25,25),
-    #                    pygame.Rect(cell.x+25,cell.y+25,25,25),pygame.Rect(cell.x-25,cell.y-25,25,25),pygame.Rect(cell.x-25,cell.y+25,25,25),pygame.Rect(cell.x+25,cell.y-25,25,25)]
-    #     for i in rectangles:
-    #         pygame.draw.rect(screen, (192,192,192), i, width=1)
-        
-    def CheckCells(cell):
+    def __init__(self, x, y):
+        self.x = x*25
+        self.y = y*25
+        self.cell = pygame.Rect(self.x, self.y, 25, 25)
+        pygame.draw.rect(screen, (192,192,192), self.cell, width=1)
+    def lifecell(x,y):
+        cell = pygame.Rect(x, y, 25, 25)
+        pygame.draw.rect(screen, (0,255,0), cell)
+        return cell
+    def allcells():
+        allcells = []
+        for x in range(40):
+            for y in range(28):
+                cell = pygame.Rect(x*25,y*25,25,25)
+                allcells.append(cell)
+        return allcells 
+    def diecell(x,y):
+        cell =pygame.Rect(x,y,25,25)
+        pygame.draw.rect(screen, (192,192,192), cell, width=1)
+        return cell
+
+async def Draw():
+    for x in range(40):
+        for y in range(28):
+            Cell(x,y)
+            pygame.display.flip()
+
+    for randcell in range(100):
+        x = randint(0,40)*25
+        y = randint(0,28)*25
+        lifecell = Cell.lifecell(x,y)
+        lifecells.append(lifecell)
+    pygame.display.flip()
+
+    
+async def game_of_life():
+    while True:
+        a = 0
+        a+=1
         x = 0
         y = 0
         neighbours = 0
-        rectangles = []
-        conditions = [pygame.Rect(x+25,y,25,25),pygame.Rect(x-25,y,25,25),pygame.Rect(x,y+25,25,25), pygame.Rect(x,y-25,25,25),
-                       pygame.Rect(x+25,y+25,25,25),pygame.Rect(x-25,y-25,25,25),pygame.Rect(x-25,y+25,25,25),pygame.Rect(x+25,y-25,25,25)]
-        for x in range(40):
-            for y in range(28):
-                rect = pygame.Rect(x*25,y*25,25,25)
-                rectangles.append(rect)
-        for i in rectangles:
-            pygame.draw.rect(screen, (192,192,192), i, width=1)
-            for a in conditions: 
-                if a.colliderect(cell.x, cell.y, 25,25):
-                    neighbours+=1
-            if neighbours>0:
-                print(neighbours)
-                neighbours = 0
-
         
-    def __init__(self,x,y):
-        self.x = x*25
-        self.y = y*25
-        self.Xcenter = self.x + 12.5
-        self.Ycenter = self.y + 12.5
-        self.r = pygame.Rect(self.x, self.y, 25, 25)
-        self.center = pygame.Rect(self.Xcenter-5, self.Ycenter-5, 10, 10)
-        pygame.draw.rect(screen, (0,255,0), self.r)
-        pygame.draw.rect(screen, (0,0,0), self.center)
-    
-async def Draw():
-    cells = []
-    for i in range(1000):
-        cell = Cell(randint(0,40), randint(0,28))
-        cells.append(cell)
-    for i in cells:
-        # print(i.x, i.y)
-        Cell.CheckCells(i)
-        
-    pygame.display.update()
+        allcells = Cell.allcells()
+        for cell in allcells:
+            conditions = [pygame.Rect(x+25,y,25,25),pygame.Rect(x-25,y,25,25),pygame.Rect(x,y+25,25,25), pygame.Rect(x,y-25,25,25),
+                    pygame.Rect(x+25,y+25,25,25),pygame.Rect(x-25,y-25,25,25),pygame.Rect(x-25,y+25,25,25),pygame.Rect(x+25,y-25,25,25)]
+            x = cell.x
+            y = cell.y
+            rect = pygame.Rect(x,y,25,25)
+            pygame.draw.rect(screen,(255,0,0),rect, width=1)
+            pygame.display.flip()
+            for condition in conditions:
+                x = cell.x
+                y = cell.y
+                pygame.draw.rect(screen,(0,0,255), condition, width=1)
+                pygame.display.flip()
+                for lifecell in lifecells:
+                    if condition.colliderect(lifecell.x, lifecell.y, 25,25):
+                        neighbours+=1
+                    else:
+                        pass
+                    if neighbours == 2 or neighbours == 3: 
+                        print(neighbours)
+                        lifecells.append(Cell.lifecell(x,y))
+                    else:
+                        try:
+                            lifecells.remove(pygame.Rect(x,y,25,25))
+                        except:
+                            pass
+                        Cell.diecell(x,y)
+                    pygame.display.flip()
+                                    
 async def checkQUIT():
     while True:
         for event in pygame.event.get():
@@ -67,5 +89,5 @@ async def checkQUIT():
                 sys.exit()
         await asyncio.sleep(0.01)
 async def main():
-    await asyncio.gather(Draw(), checkQUIT())
+    await asyncio.gather(Draw(), checkQUIT(), game_of_life())
 asyncio.run(main())
